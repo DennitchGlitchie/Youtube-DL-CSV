68c9b794ff4c2ec125874a806b418b4445f42d00
David Garges <garges@google.com>
2021-02-06 19:00:49 -0800

    Made a lot of progress with variables and arguments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Youtube-DL-CSV
+# Summary: This is a working document to develop a tool to easily download and manage youtube downloads. A program will extract URLs from this document and save audio files to desired directory with desired file name. This tool will also automatically mark when a download has been completed. 
--- Script.sh
+++ Script.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 echo Hello World
+
+FILE=$1
+#echo "The name of the File is $1";
+#echo "The name of the File is ${FILE}";
+
+while IFS="," read -r f1 f2 f3 f4 f5
+do
+	echo "$f1"
+	echo "$f2"
+	echo "$f3"
+	echo "$f4"
+	echo "$f5"
+done < $FILE
+
+
+URL="https://www.youtube.com/watch?v=aZnPjwXTap4"
+ARTIST="My_Artist"
+TITLE="Title"
+PATHMY="${ARTIST}${TITLE}.m4a"
+
+#PATHMY=/Users/garges/Desktop/Github/Youtube-DL-CSV/Output/test.m4a
+#youtube-dl -f 140 --output TEST.m4a $URL
+#youtube-dl -f 140 --output "%(title)s.%(ext)s" $URL
+
+
+youtube-dl -f 140 --output $PATHMY $URL
+
