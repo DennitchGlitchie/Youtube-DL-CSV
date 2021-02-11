0a133009a1a2c8bd2c35aede6c2bcb2fbb5c51ef
David Garges <garges@google.com>
2021-02-11 11:09:17 -0800

    Understanding that While Do expects a newline

--- Script.sh
+++ Script.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
-echo Hello World
+echo Thank You for Using My Youtube-dl tool!
 
-FILE=$1
 #echo "The name of the File is $1";
 #echo "The name of the File is ${FILE}";
 
-while IFS="," read -r f1 f2 f3 f4 f5
+while IFS="," read f1 f2 f3
 do
-	echo "$f1"
-	echo "$f2"
-	echo "$f3"
-	echo "$f4"
-	echo "$f5"
-done < $FILE
+	youtube-dl -f 140 --output "${f3}/${f2}" "$f1"
+	#ffmpeg -i "${f3}/${f2}.m4a" -c:a libmp3lame -q:a 8 output.mp3
+
+done < $1 
 
 
-URL="https://www.youtube.com/watch?v=aZnPjwXTap4"
-ARTIST="My_Artist"
-TITLE="Title"
-PATHMY="${ARTIST}${TITLE}.m4a"
+
+#URL="https://www.youtube.com/watch?v=aZnPjwXTap4"
+#ARTIST="My_Artist"
+#TITLE="Title"
+#PATHMY="${ARTIST}${TITLE}.m4a"
 
 #PATHMY=/Users/garges/Desktop/Github/Youtube-DL-CSV/Output/test.m4a
 #youtube-dl -f 140 --output TEST.m4a $URL
 #youtube-dl -f 140 --output "%(title)s.%(ext)s" $URL
 
 
-youtube-dl -f 140 --output $PATHMY $URL
+#youtube-dl -f 140 --output $PATHMY $URL
 
